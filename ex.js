7ad51e477e0f1f58e27ec7aebcde875e703948d9
mmalurea <mariusmalureanu@gmail.com>
2019-07-27 15:11:21 +0300

    Contact_form_update

---
+++ ex.js
@@ -0,0 +1,12 @@
+const myFunction = (arr, i) => {
+    for (i = 0; i < arr.length; i++) {
+        if (arr[i] > 5 && arr[i] < 100) {
+            console.log(arr[i])
+        }
+    }
+
+}
+
+var filterFunction = function(arr, myFunction) {
+
+}
